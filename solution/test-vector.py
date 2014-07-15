091696182cdc3d9ad71a98342a3aeb2d0efacc77
Kale Kundert <kale@thekunderts.net>
2014-07-15 11:36:25 -0700

    Write some really weak tests.

--- solution/test-vector.py
+++ solution/test-vector.py
@@ -1,15 +1,22 @@
+#!/usr/bin/env python2
+
 import vector
 import math
 
-for i in range(12):
-    x = math.cos(2 * math.pi * i / 12)
-    y = math.sin(2 * math.pi * i / 12)
+v1 = [36.886, 53.177, 21.887]
+v2 = [38.323, 52.817, 21.996]
+v3 = [38.493, 51.553, 22.830]
+v4 = [39.483, 50.748, 22.463]
 
-    v1 = [-1, 0, 0]
-    v2 = [0, 0, 0]
-    v3 = [x, y, 0]
+n1 = vector.normalize(v1)
+n2 = vector.normalize(v2)
 
-    print v3
-    print vector.angle(v1, v2, v3)
-    print
+v1 = n1 = 1,2,3
+v2 = n2 = 3,4,5
 
+print vector.add(v1, v2)
+print vector.subtract(v1, v2)
+print vector.normalize(v1)
+print vector.dot(n1, n2)
+print vector.cross(n1, n2)
+print vector.torsion(v1, v2, v3, v4)
