63922d89616f25dd227d721d6bb5374342139d42
Kale Kundert <kale@thekunderts.net>
2014-07-16 14:38:47 -0700

    Make stylistic changes.
    I shortened the example function to get part 4 to fit on page 6, and
    changed the source code highlighting style.  I'd like to get rid of the
    stupid-looking italic comments, but this seems difficult.

--- document/vector-functions.py
+++
@@ -1,7 +0,0 @@
-# Example function for adding vectors
-def add(v1, v2):
-    # v1 and v2 are both 3d vectors represented as tuples of floats
-    # e.g. v1 = (1.0, 2.4, -3.9)
-    return [v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]]
-
-# Write more functions to do vector math! (dot product, etc.)
