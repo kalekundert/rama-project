8f023a16abac4eba310c8ce889d1b04d94298b2a
Kale Kundert <kale@thekunderts.net>
2015-07-06 11:44:29 -0700

    Pre-prolines instead of prolines.

--- document/example-function.py
+++ document/example-function.py
@@ -1,3 +1,3 @@
 # v1 and v2 are both 3D vectors represented as tuples of floats
 def add(v1, v2):
-    return (v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2])
+    return v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]
